77ecc7cbaabe8c6e76627ba91e2d686b163be36f
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-01-09 23:16:53 -0600

    Add files via upload

---
+++ src/View/Alumns/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { NavAlum,CredentialAlumn } from '../../Components/Alumns'
+import {Footer} from '../../Components/Public'
+export default function IndexAlumn() {
+  return (
+    <div>
+      <NavAlum/>
+      <CredentialAlumn/>
+      <Footer/>
+    </div>
+  )
+}
