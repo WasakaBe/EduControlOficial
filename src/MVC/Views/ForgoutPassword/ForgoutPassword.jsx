499e1dc278178d5f40183b281a5bc2cdb06278f7
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-01-23 10:27:44 -0600

    Add files via upload

---
+++ src/MVC/Views/ForgoutPassword/ForgoutPassword.jsx
@@ -0,0 +1,97 @@
+import React, { useState } from 'react';
+import { Footer, Navbar } from '../../../Components/Public';
+import { useNavigate } from 'react-router-dom';
+import './ForgoutPassword.css';
+
+export default function ForgoutPassword() {
+  const navigate = useNavigate();
+  const [correo, setCorreo] = useState('');
+  const [correoValido, setCorreoValido] = useState(true);
+  const [correoEncontrado, setCorreoEncontrado] = useState(false);
+  const [token, setToken] = useState('');
+  const [error, setError] = useState('');
+
+  const handleVerifyCorreo = async () => {
+    if (correoValido) {
+      try {
+        const response = await fetch('http://192.168.1.24:40009/check-email', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({ correo_users: correo }),
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+
+          if (data.exists) {
+            setCorreoEncontrado(true);
+            
+            const tokenResponse = await fetch('http://192.168.1.24:40009/get-token', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ correo_users: correo }),
+            });
+
+            if (tokenResponse.ok) {
+              const tokenData = await tokenResponse.json();
+              setToken(tokenData.token_users);
+            } else {
+              setError('Error al obtener el token. Inténtelo nuevamente.');
+            }
+          } else {
+            setError('Correo no encontrado. Intente con otro correo.');
+          }
+        } else {
+          setError('Error al verificar el correo. Inténtelo nuevamente.');
+        }
+      } catch (error) {
+        setError('Error al conectar con la API. Inténtelo nuevamente.');
+      }
+    }
+  };
+
+  const handleReturnToLogin = () => {
+    navigate(`/ForgoutToken/${correo}`);
+  };
+
+  return (
+    <div>
+      <Navbar />
+      <div className='container-forgout'>
+        <div className='forgout-form'>
+          <h2>Recuperar Contraseña</h2>
+
+          {correoEncontrado ? (
+            <>
+              <p>Correo Encontrado :</p>
+              <p>{correo}</p>
+              <p>Copie el TOKEN: {token}</p>
+              <button onClick={handleReturnToLogin}>Siguiente</button>
+            </>
+          ) : null}
+
+          {!correoEncontrado && (
+            <>
+              <label>Correo Electrónico:</label>
+              <input
+                type='email'
+                value={correo}
+                onChange={(e) => setCorreo(e.target.value)}
+                onBlur={() => setCorreoValido(/^\S+@\S+\.\S+$/.test(correo))}
+                style={{ border: correoValido ? '1px solid white' : '1px solid red' }}
+              />
+              {!correoValido && <p>Ingrese correctamente el correo.</p>}
+              {error && <p style={{ color: 'red' }}>{error}</p>}
+              <button onClick={handleVerifyCorreo}>Verificar Correo</button>
+            </>
+          )}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
