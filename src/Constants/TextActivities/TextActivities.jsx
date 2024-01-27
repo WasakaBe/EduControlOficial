238f8bf1b16ec28e3f3e3d3eca01d7b565a98691
Wasaka BegeinTv <127060500+WasakaBe@users.noreply.github.com>
2024-01-26 23:34:46 -0600

    Delete src/Constants directory

--- src/Constants/TextActivities/TextActivities.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './TextActivities.css'
-export default function TextActivities() {
-  return (
-    <div className='container_Text_Actv'>
-        <h3> Actividades Escolares</h3>
-    </div>
-  )
-}
